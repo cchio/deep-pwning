66f2eececa020045dd189386dd69d5b8d290ed8c
Clarence Chio <u608@intrafile.com>
2016-06-24 15:05:33 -0700

    Fixed indentation for all files.

--- pwnml/models/lenet5.py
+++ pwnml/models/lenet5.py
@@ -19,78 +19,75 @@ DATA_flavor = tf.float32
 
 class LeNet5:
 
-  def conv2d(self, data, weight):
-    return tf.nn.conv2d(data,
-                        weight,
-                        strides=[1, 1, 1, 1],
-                        padding='SAME')
+    def conv2d(self, data, weight):
+        return tf.nn.conv2d(data,
+                            weight,
+                            strides=[1, 1, 1, 1],
+                            padding='SAME')
 
-  def max_pool(self, data):
-    return tf.nn.max_pool(data,
-                          ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1],
-                          padding='SAME')
+    def max_pool(self, data):
+        return tf.nn.max_pool(data,
+                              ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1],
+                              padding='SAME')
 
-  def variable(self, flavor, shape):
-    if flavor == 'W':
-      return tf.Variable(
-        tf.truncated_normal(shape,
-                            stddev=0.1,
-                            seed=SEED, dtype=DATA_flavor))      
-    elif flavor == 'b':
-      return tf.Variable(tf.constant(0.1, shape=shape), 
-                                     dtype=DATA_flavor)
-    else:
-      return None
+    def variable(self, flavor, shape):
+        if flavor == 'W':
+            return tf.Variable(
+                tf.truncated_normal(shape,
+                                    stddev=0.1,
+                                    seed=SEED, 
+                                    dtype=DATA_flavor))      
+        elif flavor == 'b':
+            return tf.Variable(tf.constant(0.1, shape=shape), 
+                               dtype=DATA_flavor)
+        else:
+            return None
 
-  def train_input_placeholders(self):
-    x = tf.placeholder(
-      DATA_flavor,
-      shape=[None, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS],
-      name="x")
-    y_ = tf.placeholder(tf.int64, shape=[None,], name="y_")
-    return x, y_
+    def train_input_placeholders(self):
+        x = tf.placeholder(DATA_flavor,
+                           shape=[None, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS],
+                           name="x")
+        y_ = tf.placeholder(tf.int64, shape=[None,], name="y_")
+        return x, y_
 
-  def model(self, data):
+    def model(self, data):
+        conv1_W = self.variable('W', [5, 5, NUM_CHANNELS, 32])
+        conv1_b = self.variable('b', [32])
+        conv1 = tf.nn.relu(tf.nn.bias_add(self.conv2d(data, conv1_W), conv1_b))
 
-    conv1_W = self.variable('W', [5, 5, NUM_CHANNELS, 32])
-    conv1_b = self.variable('b', [32])
-    conv1 = tf.nn.relu(tf.nn.bias_add(self.conv2d(data, conv1_W), conv1_b))
+        pool1 = self.max_pool(conv1)
 
-    pool1 = self.max_pool(conv1)
+        conv2_W = self.variable('W', [5, 5, 32, 64])
+        conv2_b = self.variable('b', [64])
+        conv2 = tf.nn.relu(tf.nn.bias_add(self.conv2d(pool1, conv2_W), conv2_b))
 
-    conv2_W = self.variable('W', [5, 5, 32, 64])
-    conv2_b = self.variable('b', [64])
-    conv2 = tf.nn.relu(tf.nn.bias_add(self.conv2d(pool1, conv2_W), conv2_b))
+        pool2 = self.max_pool(conv2)
 
-    pool2 = self.max_pool(conv2)
+        pool2_shape = pool2.get_shape().as_list()
+        pool2_reshaped = tf.reshape(pool2,
+            [-1, pool2_shape[1] * pool2_shape[2] * pool2_shape[3]])
 
-    pool2_shape = pool2.get_shape().as_list()
-    pool2_reshaped = tf.reshape(pool2,
-      [-1, pool2_shape[1] * pool2_shape[2] * pool2_shape[3]])
+        fc1_W = self.variable('W', [IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512])
+        fc1_b = self.variable('b', [512])
+        fc1 = tf.nn.relu(tf.matmul(pool2_reshaped, fc1_W) + fc1_b)
 
-    fc1_W = self.variable('W', [IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512])
-    fc1_b = self.variable('b', [512])
-    fc1 = tf.nn.relu(tf.matmul(pool2_reshaped, fc1_W) + fc1_b)
+        keep_prob = tf.placeholder("float", name="keep_prob")
+        fc1_dropout = tf.nn.dropout(fc1, keep_prob, seed=SEED)
 
-    keep_prob = tf.placeholder("float", name="keep_prob")
-    fc1_dropout = tf.nn.dropout(fc1, keep_prob, seed=SEED)
+        fc2_W = self.variable('W', [512, NUM_LABELS])
+        fc2_b = self.variable('b', [NUM_LABELS])
 
-    fc2_W = self.variable('W', [512, NUM_LABELS])
-    fc2_b = self.variable('b', [NUM_LABELS])
-
-    logits = tf.matmul(fc1_dropout, fc2_W) + fc2_b
-    y_conv = tf.nn.softmax(logits)
-
-    param_dict = {
-      'conv1_W': conv1_W,
-      'conv1_b': conv1_b,
-      'conv2_W': conv2_W,
-      'conv2_b': conv2_b,
-      'fc1_W': fc1_W,
-      'fc1_b': fc1_b,
-      'fc2_W': fc2_W,
-      'fc2_b': fc2_b
-    }
-
-    return y_conv, logits, keep_prob, param_dict
+        logits = tf.matmul(fc1_dropout, fc2_W) + fc2_b
+        y_conv = tf.nn.softmax(logits)
+        param_dict = {
+            'conv1_W': conv1_W,
+            'conv1_b': conv1_b,
+            'conv2_W': conv2_W,
+            'conv2_b': conv2_b,
+            'fc1_W': fc1_W,
+            'fc1_b': fc1_b,
+            'fc2_W': fc2_W,
+            'fc2_b': fc2_b
+        }
+        return y_conv, logits, keep_prob, param_dict
--- pwnml/utils/utils.py
+++ pwnml/utils/utils.py
@@ -29,86 +29,85 @@ DATA_TYPE = tf.float32
 
 
 def maybe_download(filename):
-  """Download the data from Yann's website, unless it's already here."""
-  if not tf.gfile.Exists(WORK_DIRECTORY):
-    tf.gfile.MakeDirs(WORK_DIRECTORY)
-  filepath = os.path.join(WORK_DIRECTORY, filename)
-  if not tf.gfile.Exists(filepath):
-    filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
-    with tf.gfile.GFile(filepath) as f:
-      size = f.Size()
-    print('Successfully downloaded', filename, size, 'bytes.')
-  return filepath
+    """Download the data from Yann's website, unless it's already here."""
+    if not tf.gfile.Exists(WORK_DIRECTORY):
+        tf.gfile.MakeDirs(WORK_DIRECTORY)
+    filepath = os.path.join(WORK_DIRECTORY, filename)
+    if not tf.gfile.Exists(filepath):
+        filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
+        with tf.gfile.GFile(filepath) as f:
+            size = f.Size()
+        print('Successfully downloaded', filename, size, 'bytes.')
+    return filepath
 
 def extract_data(filename, num_images):
-  """Extract the images into a 4D tensor [image index, y, x, channels].
-
-  Values are rescaled from [0, 255] down to [-0.5, 0.5].
-  """
-  print('Extracting', filename)
-  with gzip.open(filename) as bytestream:
-    bytestream.read(16)
-    buf = bytestream.read(IMAGE_SIZE * IMAGE_SIZE * num_images)
-    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-    data = (data - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH
-    data = data.reshape(num_images, IMAGE_SIZE, IMAGE_SIZE, 1)
+    """Extract the images into a 4D tensor [image index, y, x, channels].
+       Values are rescaled from [0, 255] down to [-0.5, 0.5]."""
+    print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        bytestream.read(16)
+        buf = bytestream.read(IMAGE_SIZE * IMAGE_SIZE * num_images)
+        data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+        data = (data - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH
+        data = data.reshape(num_images, IMAGE_SIZE, IMAGE_SIZE, 1)
     return data
 
 def extract_labels(filename, num_images):
-  """Extract the labels into a vector of int64 label IDs."""
-  print('Extracting', filename)
-  with gzip.open(filename) as bytestream:
-    bytestream.read(8)
-    buf = bytestream.read(1 * num_images)
-    labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
-  return labels
+    """Extract the labels into a vector of int64 label IDs."""
+    print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        bytestream.read(8)
+        buf = bytestream.read(1 * num_images)
+        labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+    return labels
 
 def fake_data(num_images):
-  """Generate a fake dataset that matches the dimensions of MNIST."""
-  data = np.ndarray(
-      shape=(num_images, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS),
-      dtype=np.float32)
-  labels = np.zeros(shape=(num_images,), dtype=np.int64)
-  for image in xrange(num_images):
-    label = image % 2
-    data[image, :, :, 0] = label - 0.5
-    labels[image] = label
-  return data, labels
+    """Generate a fake dataset that matches the dimensions of MNIST."""
+    data = np.ndarray(
+        shape=(num_images, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS),
+        dtype=np.float32)
+    labels = np.zeros(shape=(num_images,), dtype=np.int64)
+    for image in xrange(num_images):
+        label = image % 2
+        data[image, :, :, 0] = label - 0.5
+        labels[image] = label
+    return data, labels
 
 def error_rate(predictions, labels):
-  """Return the error rate based on dense predictions and sparse labels."""
-  return 100.0 - (
-      100.0 *
-      np.sum(np.argmax(predictions, 1) == labels) /
-      predictions.shape[0])
+    """Return the error rate based on dense predictions and sparse labels."""
+    return 100.0 - (
+        100.0 *
+        np.sum(np.argmax(predictions, 1) == labels) /
+        predictions.shape[0])
 
 def ensure_dir(f):
-  d = os.path.dirname(f)
-  if not os.path.exists(d):
-    os.makedirs(d)
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
 
 def compare_mnist_digits(im1, im2, im1_label, im2_label, idx, perturbation=.0, out_dir='', method=''):
-  """ Plot 2 MNIST images side by side."""
-  ensure_dir('{0}/fooled/{1}/'.format(out_dir, perturbation))
-  ensure_dir('{0}/not-fooled/{1}/'.format(out_dir, perturbation))
+    """ Plot 2 MNIST images side by side."""
+    ensure_dir('{0}/fooled/{1}/'.format(out_dir, perturbation))
+    ensure_dir('{0}/not-fooled/{1}/'.format(out_dir, perturbation))
 
-  fig = plt.figure()
-  ax = fig.add_subplot(1, 2, 1)
-  ax.matshow(im1, cmap='Greys')
-  plt.title('im1 predicted label: ' + str(im1_label))
-  plt.xticks(np.array([]))
-  plt.yticks(np.array([]))
-  ax = fig.add_subplot(1, 2, 2)
-  ax.matshow(im2, cmap='Greys')
-  plt.title('im2 predicted label: ' + str(im2_label))
-  plt.xticks(np.array([]))
-  plt.yticks(np.array([]))
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1)
+    ax.matshow(im1, cmap='Greys')
+    plt.title('im1 predicted label: ' + str(im1_label))
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    ax = fig.add_subplot(1, 2, 2)
+    ax.matshow(im2, cmap='Greys')
+    plt.title('im2 predicted label: ' + str(im2_label))
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
 
-  if not out_dir:
-    plt.show()
-  else:
-    if im1_label != im2_label:
-      out_path = '{0}/fooled/{1}/{2}-{3}.png'.format(out_dir, perturbation, method, idx)
+    if not out_dir:
+        plt.show()
     else:
-      out_path = '{0}/not-fooled/{1}/{2}-{3}.png'.format(out_dir, perturbation, method, idx)
-    fig.savefig(out_path)
+        if im1_label != im2_label:
+            out_path = '{0}/fooled/{1}/{2}-{3}.png'.format(out_dir, perturbation, method, idx)
+        else:
+            out_path = '{0}/not-fooled/{1}/{2}-{3}.png'.format(out_dir, perturbation, method, idx)
+        fig.savefig(out_path)
+        plt.close(fig)
